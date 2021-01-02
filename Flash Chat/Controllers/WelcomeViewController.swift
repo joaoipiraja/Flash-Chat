f99721c25bf5db8f102dd796e1b25be273af15c0
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-02 11:04:09 -0300

    Configure Cocoapods in Project/ Add CLTypingLabel Framework

--- Flash Chat/Controllers/WelcomeViewController.swift
+++ Flash Chat/Controllers/WelcomeViewController.swift
@@ -6,36 +6,18 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
-    
+    @IBOutlet weak var titleLabel:CLTypingLabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        animationTitle()
+        titleLabel.text = "⚡️FlashChat"
     }
     
     
 }
 
-
-extension WelcomeViewController{
-    func animationTitle(){
-        
-        let titleText = self.titleLabel.text ?? ""
-        self.titleLabel.text = ""
-        var characterIndex:Double = 1.0
-        for letter in titleText{
-            
-            Timer.scheduledTimer(withTimeInterval: 0.1*characterIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            
-            characterIndex += 1.0
-            
-            
-        }
-    }
-}
 
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '14.2'
+
+target 'Flash Chat' do
+  use_frameworks!
+  pod 'CLTypingLabel'
+
+end
